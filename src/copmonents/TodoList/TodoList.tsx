4c700faa81d7cfab645d616c58c40c6d84ba22a0
Serhii30 <atek3001@gmail.com>
2022-01-18 10:12:16 +0200

    search task

--- src/copmonents/TodoList/TodoList.tsx
+++ src/copmonents/TodoList/TodoList.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useMemo, useState } from 'react'
 import TodoItem from '../TodoItem/TodoItem'
 import { useTypedSelector } from '../hooks/useTypedSelector'
 import { Todo, visibilityFilters } from '../../types/todo'
 import s from './TodoList.module.scss'
+import Input from 'copmonents/UI/Input/Input'
 
 const TodoList: React.FC = () => {
+    const [searchValue, setSearchValue] = useState('')
+
     const todos = useTypedSelector(state => state.todos.todos)
     const filter = useTypedSelector(state => state.filter.filter)
 
@@ -19,16 +22,29 @@ const TodoList: React.FC = () => {
         }
     }
 
-    const visibleTodos = filterTasks(todos, filter)
+    const sortedTask = useMemo(() => {
+        return todos.filter(task => task.title.toLowerCase().includes(searchValue.toLowerCase()))
+    }, [todos, searchValue])
 
-    if (todos.length === 0) {
+    const visibleTodos = filterTasks(sortedTask, filter)
+
+    if (!todos.length) {
         return <p className={s.noTask}>завдань не має!</p>
     }
 
     return (
-        <ul>
-            {visibleTodos.map((todo: Todo) => <TodoItem key={todo.id} todo={todo} />)}
-        </ul>
+        <div>
+            <Input
+                inputClass='searchInput'
+                value={searchValue} 
+                type='title'
+                onChange={e => setSearchValue(e.target.value)}
+                placeholder='Знайти завдання...'
+            />
+            <ul>
+                {visibleTodos.map((todo: Todo) => <TodoItem key={todo.id} todo={todo} />)}
+            </ul>
+        </div>
     )
 }
 
